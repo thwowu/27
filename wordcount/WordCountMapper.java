82e52726bbbd80b3856cebac46bc8bf2793244f0
thwowu <tse-hao.wu@student.ecp.fr>
2017-04-21 15:28:29 +0200

    Update WordCountMapper.java

--- wordcount/WordCountMapper.java
+++ wordcount/WordCountMapper.java
@@ -4,9 +4,9 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class Exercisemapper extends Mapper<LongWritable,Text,Text,Text>
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable>
 {
-    public void map(LongWritable key,Text value,Context context) throws IOException,InterruptedException
+    public void map(LongWritable key, Text value, Context context) throws IOException,InterruptedException
     {
         for (String token: value.toString().split("\\s+")) {
             word.set(token);
