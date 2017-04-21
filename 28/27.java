648b2fdeb587be04aa4adb6ad9a0ed1272631cdc
thwowu <tse-hao.wu@student.ecp.fr>
2017-04-21 08:54:26 -0700

    Update 27.java

--- 28/27.java
+++ 28/27.java
@@ -4,11 +4,15 @@ import java.io.*;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.*;
 
-public class Question28 {
 
-	public static void main(String[] args) throws IOException {
-		
-		Path filename = new Path(args[0]);
+import org.apache.hadoop.fs.FSDataInputStream;
+
+
+public class Question27 {
+
+    public static void main(String[] args) throws IOException  {
+
+    	Path filename = new Path(args[0]);
 		
 		//Open the file
 		Configuration conf = new Configuration();
@@ -24,8 +28,8 @@ public class Question28 {
 			String line = br.readLine();
 			while (line !=null){
 				// Process of the current line
-				String[] country = line.split(;);
-        System.out.println("Tree: " + country[11] + ", year=" + country[5] + ", height=" + country[6]);
+				String[] country = line.split(";");
+				System.out.println("Tree: " + country[11] + ", year=" + country[5] + ", height=" + country[6]);
 
 				// go to the next line
 				line = br.readLine();
@@ -36,9 +40,40 @@ public class Question28 {
 			inStream.close();
 			fs.close();
 		}
+		
+    	/*
+        // String csvFile = "/home/cloudera/BigData/Lab1/arbres.csv";
+        String csvFile = args[0];
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ";";
 
-		
-		
-	}
+        try {
+
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] country = line.split(cvsSplitBy);
+
+                System.out.println("Tree' " + country[11] + " , Year =" + country[5]  + " , height =" + country[6]);
+
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+*/
+    }
 
 }
