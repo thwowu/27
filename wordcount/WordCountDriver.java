ec2b21a17177c0d2b31d2f429e387ed9560e529a
thwowu <tse-hao.wu@student.ecp.fr>
2017-04-21 15:34:12 +0200

    Update WordCountDriver.java

--- wordcount/WordCountDriver.java
+++ wordcount/WordCountDriver.java
@@ -17,15 +17,16 @@ public class WordCountDriver {
         FileOutputFormat.setOutputPath(job,new Path(args[1]));
 
         job.setMapperClass(WordCountMapper.class);
-
         job.setReducerClass(WordCountReducer.class);
-
+        job.setNumReduceTasks(1);
 
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
-        job.setNumReduceTasks(1);
+        job.setOutputValueClass(IntWritable.class);
+        
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+        
         System.exit(job.waitForCompletion(true) ? 0 : 1);
 
     }
-
 }
