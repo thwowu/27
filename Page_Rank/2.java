38ef122bfea8ec7c166be417d69683648395b225
thwowu <tse-hao.wu@student.ecp.fr>
2017-05-02 19:31:12 +0200

    Create 2.java

---
+++ Page_Rank/2.java
@@ -0,0 +1,124 @@
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Cluster;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class PageRank
+{
+	static enum PageCount{
+		Count,TotalPR
+	}
+	public static class PageRankMapper extends Mapper<Object, Text, Text, Text>
+	{
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException
+		{
+			context.getCounter(PageCount.Count).increment(1);
+			String[] kv = value.toString().split("\t");
+			String _key = kv[0];
+			String _value = kv[1];
+			String _PRnLink[] = _value.split(" ");
+			String pr = _PRnLink[0];
+			String link = _PRnLink[1];
+			context.write(new Text(_key), new Text(link));
+
+			String site[] = link.split(",");
+			float score = Float.valueOf(pr)/(site.length)*1.0f;
+			for(int i = 0 ; i < site.length ; i++)
+			{
+				context.write(new Text(site[i]), new Text(String.valueOf(score)));
+			}	
+
+		}
+	}
+ 
+	public static class PageRankReducer extends Reducer<Text, Text, Text, Text>
+	{
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException
+		{
+			StringBuilder sb = new StringBuilder();
+			float factor = 0.85f;
+			float pr = 0f;
+			for(Text f : values)
+			{				
+				String value = f.toString();
+				int s = value.indexOf(".");
+				if(s != -1)
+				{
+					pr += Float.valueOf(value);
+				}else{
+					String site[] = value.split(",");
+					int _len = site.length;
+					for(int k = 0 ; k < _len ;k++)
+					{
+						sb.append(site[k]);
+						sb.append(",");
+					}
+				}
+			}
+
+			pr = ((1-factor)+(factor*(pr)));
+			context.getCounter(PageCount.TotalPR).increment((int)(pr*1000));
+			String output = pr+" "+sb.toString();
+			context.write(key, new Text(output));
+		}
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		String input;
+		String output;
+		int threshold = 1000;
+		int iteration = 0;
+		int iterationLimit = 100;
+		boolean status = false;
+		
+		while(iteration < iterationLimit)
+		{
+			if((iteration % 2) == 0)
+			{
+				input = "/pagerank_output/p*";
+				output = "/pagerank_output2/";
+			}else{
+				input = "/pagerank_output2/p*";
+				output = "/pagerank_output/";
+			}
+
+			Configuration conf = new Configuration();	
+			
+			FileSystem fs = FileSystem.get(conf);
+			fs.delete(new Path(output), true);
+			
+			Job job = Job.getInstance(new Cluster(conf));
+			job.setJobName("PageRank");
+			job.setJarByClass(PageRank.class);
+			job.setMapperClass(PageRankMapper.class);
+			job.setReducerClass(PageRankReducer.class);
+			job.setOutputKeyClass(Text.class);
+			job.setOutputValueClass(Text.class);
+			TextInputFormat.addInputPath(job, new Path(input));
+			TextOutputFormat.setOutputPath(job, new Path(output));
+			status = job.waitForCompletion(true);
+			iteration++;
+			
+			long count = job.getCounters().findCounter(PageCount.Count).getValue();
+			long total_pr = job.getCounters().findCounter(PageCount.TotalPR).getValue();
+			System.out.println("PageCount:"+count);
+			System.out.println("TotalPR:"+total_pr);
+			double per_pr = total_pr/(count*1.0d);
+			System.out.println("Per PR:"+per_pr);
+			if((int)per_pr == threshold)
+			{
+				System.out.println("Iteration:"+iteration);
+				break;
+			}
+		}
+		System.exit(status?0:1);
+	}
+}
