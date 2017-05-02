b48a0bccc9bc77e1252c69f2164b426a881dec36
thwowu <tse-hao.wu@student.ecp.fr>
2017-05-02 19:30:29 +0200

    Update 1.java

--- Page_Rank/1.java
+++ Page_Rank/1.java
@@ -20,6 +20,22 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.HashSet;
+import java.util.Set;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+
 
 public class MDP01A03 extends Configured implements Tool {
 	public static void main(String[] args) throws Exception {
@@ -28,49 +44,37 @@ public class MDP01A03 extends Configured implements Tool {
 		System.exit(res);
 		   }
 
+	public static NumberFormat NF = new DecimalFormat("00");
+   	public static Set<String> NODES = new HashSet<String>();
+    	public static String LINKS_SEPARATOR = "|";
+	
 	@Override
 	public int run(String[] args) throws Exception {
 	      System.out.println(Arrays.toString(args));
 	      Job job = new Job(getConf(), "MDP01A03");
 	      job.setJarByClass(MDP01A03.class);
-	      job.setOutputKeyClass(Text.class);
-	      job.setOutputValueClass(IntWritable.class);
 
+	      job.setOutputKeyClass(Text.class);
+	      job.setOutputValueClass(Text.class);
+	      job.setMapOutputKeyClass(Text.class);
+              job.setMapOutputValueClass(Text.class);
+		
 	      job.setMapperClass(Map.class);
 	      job.setReducerClass(Reduce.class);
-	      job.setNumReduceTasks(10);
+	      job.setNumReduceTasks(1);
 	      job.setCombinerClass(Reduce.class);
 	      
 	      job.setInputFormatClass(TextInputFormat.class);
 	      job.setOutputFormatClass(TextOutputFormat.class);
-	      
-	      FileOutputFormat.setCompressOutput(job, true);
-	      FileOutputFormat.setOutputCompressorClass(job, org.apache.hadoop.io.compress.GzipCodec.class);
-	      // choices at page 129, Hadoop; Definitive Guide 4th edition
-	      /* Worked:
-	      org.apache.hadoop.io.compress.BZip2Codec
-	      org.apache.hadoop.io.compress.GzipCodec
-	      org.apache.hadoop.io.compress.DefaultCodec
-	      
-	      not working:
-	      org.apache.hadoop.io.compress.SnappyCodec ("this version of libhadoop was built without " + "snappy support")
-	      com.hadoop.compression.lzo.LzopCodec
-	      org.apache.hadoop.io.compress.Lz4Codec
-	      org.apache.hadoop.io.compress.DefaultCodec
-	      */
-	      
+
 	      FileInputFormat.addInputPath(job, new Path(args[0]));
 	      FileOutputFormat.setOutputPath(job, new Path(args[1]));
 	      job.getConfiguration().set("mapreduce.output.textoutputformat.separator", ",");
 	      
-	      /* Delete output file Path if already exists 
-	       * idea from http://unmeshasreeveni.blogspot.com/2014/04/code-for-deleting-existing-output.html */
 	      FileSystem fs = FileSystem.newInstance(getConf());
-
 	      if (fs.exists(new Path(args[1]))) {
 				fs.delete(new Path(args[1]), true);
 			}
-	      // ends here
 	      
 	      
 	      job.waitForCompletion(true);
@@ -78,42 +82,47 @@ public class MDP01A03 extends Configured implements Tool {
 	      return 0;
 	}
 
-	 public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
-	      private final static IntWritable ONE = new IntWritable(1);
-	      private Text word = new Text();
-
+	 public static class Map extends Mapper<LongWritable, Text, Text, Text> {
+		 
 	      @Override
 	      public void map(LongWritable key, Text value, Context context)
 	              throws IOException, InterruptedException {
-	    	  for (String token: value.toString().split("\\s*\\b\\s*")) {
-		        	 token = token.trim();
-		        	 Pattern p = Pattern.compile("^[a-zA-Z0-9_]");
-			    	 Matcher m = p.matcher(token.toLowerCase());
-			    	 
-		        	 if ( token.isEmpty() ) {
-		                 continue;
-		             } 
-		            	 if (!m.find())
-		            	 {
-		            		 continue;}
-		            word.set(token.toLowerCase());
-		            context.write(word, ONE);
-	         }
+		      if (value.charAt(0) != '#') {
+            
+            int tabIndex = value.find("\t");
+            String nodeA = Text.decode(value.getBytes(), 0, tabIndex);
+            String nodeB = Text.decode(value.getBytes(), tabIndex + 1, value.getLength() - (tabIndex + 1));
+            context.write(new Text(nodeA), new Text(nodeB));
+            
+            // add the current source node to the node list so we can 
+            // compute the total amount of nodes of our graph in Job#2
+            // adding to a HashSet so there will not be duplicate nodes to the list.
+
+            PageRank.NODES.add(nodeA);
+
+
+            // also add the target node to the same list: we may have a target node 
+            // with no outlinks (so it will never be parsed as source)
+            PageRank.NODES.add(nodeB);
+            }
 	      }
 	   }
 
-	   public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+	   public static class Reduce extends Reducer<Text, Text, Text, Text> {
+	      
 	      @Override
-	      public void reduce(Text key, Iterable<IntWritable> values, Context context)
+	      public void reduce(Text key, Iterable<Text> values, Context context)
 	              throws IOException, InterruptedException {
-	         int sum = 0;
-	         for (IntWritable val : values) {
-	            sum += val.get();
-	         }
-	         if (sum > 4000){
-	        	 context.write(key, new IntWritable(sum));
-	         }
-	      }
-	   }
+	      boolean first = true;
+	      String links = (PageRank.DAMPING / PageRank.NODES.size()) + " ";
+
+	      for (Text value : values) {
+		if (!first) 
+		     links += ",";
+		links += value.toString();
+		first = false;}
+		context.write(key, new Text(links)); }
+	    }
+   }
 
 }
